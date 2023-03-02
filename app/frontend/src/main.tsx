99970c11613a5625ad4d2bb4e0ced786e7e88cfc
rafaelftourinho <rafaelftourinho@gmail.com>
2023-03-02 16:38:53 -0300

    feat: install dependencies and create a Home

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
-import './index.css'
+import Home from './pages/Home'
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />,
   </React.StrictMode>,
 )
