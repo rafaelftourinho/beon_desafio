770c85fd43c5e0c82d25df060e1b5f29dc70858a
rafaelftourinho <rafaelftourinho@gmail.com>
2023-03-03 23:17:21 -0300

    feat: adding security for backend
    adding helmet for security and morgan for logs

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 import router from "./router/books.routes";
 
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
 app.use('/books', router);
 
 export default app;
